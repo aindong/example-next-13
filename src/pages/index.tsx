30afff4aac16a6b8057aa01f3cbe7c53df4e9123
Alleo Indong <alleo.indong@gmail.com>
2023-06-30 12:25:27 +0800

    testing env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,15 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const testEnv = process.env.NEXT_PUBLIC_TEST || ""
+
 export default function Home() {
+  
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      Another deployment
+      Another deployment {testEnv}
     </main>
   )
 }
